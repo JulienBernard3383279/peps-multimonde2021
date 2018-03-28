4485f3aa7b8f70a31c73b6ddd4e28fa2926c6c7d
soufiane <soufiane.namasse@grenoble-inp.org>
2018-03-28 13:24:10 +0200

    add fichiers manquant

---
+++ PEPS-Beta/PEPS-Beta/ViewModels/HistoricalChartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Helpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PEPS_Beta.ViewModels
+{
+    public class HistoricalChartViewModel
+    {
+        public Chart Chart { get; set; }
+    }
+}
